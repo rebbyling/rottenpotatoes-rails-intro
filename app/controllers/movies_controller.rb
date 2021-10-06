d4c37898db36b9a9f8c15d2eb6af63d8af28b349
Ling Zi Wei Rebecca Rebecca <1004413@mymail.sutd.edu.sg>
2021-10-06 12:31:57 +0000

    part 1 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,7 +29,7 @@ class MoviesController < ApplicationController
       flash.keep
       redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
-    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering).all
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
   end
 
   def new
